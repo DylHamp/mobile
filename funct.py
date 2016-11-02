5e1467d1efd5f3eac877534c5e5144c6da878fa2
DylHamp <178565@gmail.com>
2016-11-02 11:35:24 -0500

    Upgraded friend()

--- funct.py
+++ funct.py
@@ -4,7 +4,15 @@ def friend():
 	rel = input("(y/n)? ")
 	if rel == "y":
 		print("Yay! We are friends!")
-	if rel == "n":
+		print("What can I do for you friend?")
+		help = input()
+		if "lonely" in help:
+			print("No worries, I am always with you")
+		if "sad" in help:
+                	print("Then I shall cheer you up!")
+		else:
+			print("Maybe someother time")
+	elif rel == "n":
 		print("Fuck you")
 	else:
 		print("Sorry I didn't understand you...")
