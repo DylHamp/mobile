7cad354284346a7d437f87e8943c0331ea7c5e34
DylHamp <178565@gmail.com>
2017-01-31 10:13:32 -0600

    added stats project

--- friend.py
+++ friend.py
@@ -1,3 +1,21 @@
+def help():
+	req = "none"
+	while req != "bye":
+		print("What can I do for you " + adj[1] + "?")
+		req = input()
+		if "die" in req:
+			print("Jokes on you I'm not even alive! " + adj[0] + ".")
+		elif "money" in req:
+			print("MONEY MONEY MONEY... MONEY!... MONEY")
+		else:
+			print("Sorry I am not google... ask me something I know.")
+def game():
+	print("What kind of game do you want to play?")
+	game = input()
+	if "adventure" in game:
+		print("adventure()")
+	else:
+		print("I don't know that game. Maybe someother time")
 name = input("Hi, what is your name? ")
 print("Hello " + name + ", will you be my friend?")
 rel = input("(y/n)? ")
@@ -7,14 +25,13 @@ if rel == "y":
 if rel == "n":
 	print("Well fuck you then.")
 	adj = ['You dick', 'poop head']
-req = "none"
-while req != "bye":
-	print("What can I do for you " + adj[1] + "?")
-	req = input()
-	if "die" in req:
-		print("Jokes on you I'm not even alive! " + adj[0] + ".")
-	elif "money" in req:
-		print("MONEY MONEY MONEY... MONEY!... MONEY")
+topic = "none"
+while topic != "bye":
+	topic = input("What do you want to talk about " + adj[1] + "? ")
+	if "help" in topic:
+		help()
+	elif "game" in topic:
+		game()
 	else:
-		print("I'm not google... I am not made for that.")
+		print("How about something else?")
 print("Later " + adj[1] + ".")
